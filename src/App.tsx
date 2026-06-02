9d94d667a39fc9c09d8dd0590dc48fe43a9a9855
Rafaela Santos <rafaelavsantosdev@gmail.com>
2026-06-02 09:44:40 -0300

    feat: footer implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
 import About from "./components/Main/About/About"
 import Projects from "./components/Main/Projects/Projects"
@@ -10,6 +11,7 @@ function App() {
       <About />
       <Skills />
       <Projects />
+      <Footer />
     </>
   )
 }
