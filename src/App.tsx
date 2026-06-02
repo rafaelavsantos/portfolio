314c2cf935ecfc2a1acc12de4939b97b7f26dc4e
Rafaela Santos <rafaelavsantosdev@gmail.com>
2026-06-02 09:36:13 -0300

    feat: about implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header"
+import About from "./components/Main/About/About"
 
 function App() {
   return (
     <>
       <Header />
+      <About />
     </>
   )
 }
